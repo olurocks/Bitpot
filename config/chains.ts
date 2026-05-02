ab33563e59f6bcb660a1ed37311fec2a2045e863
olur0cks <olurocks1@gmail.com>
2026-05-02 14:10:54 +0200

    implement working tree

---
+++ config/chains.ts
@@ -0,0 +1,23 @@
+import { defineChain } from "viem";
+
+export const mezoTestnet = defineChain({
+  id: 31611,
+  name: "Mezo Testnet",
+  nativeCurrency: {
+    name: "Bitcoin",
+    symbol: "BTC",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: [process.env.NEXT_PUBLIC_RPC_URL || "https://rpc.test.mezo.org"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Mezo Explorer",
+      url: "https://explorer.test.mezo.org",
+    },
+  },
+  testnet: true,
+});
